51554277220fc9badcbb887daf7b2c7c5f2c47bb
HikPb <p.ngocba@gmail.com>
2020-08-11 15:29:13 +0700

    Tutorial Part 10: Testing a Django web application

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,11 +7,11 @@ from django.utils.translation import ugettext_lazy as _
 
 from catalog.models import BookInstance
 
-# class RenewBookForm(forms.Form):
-#     renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+# class RenewBookModelForm(forms.Form):
+#     due_back = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
 
-#     def clean_renewal_date(self):
-#         data = self.cleaned_data['renewal_date']
+#     def clean_due_back(self):
+#         data = self.cleaned_data['due_back']
         
 #         # Check if a date is not in the past. 
 #         if data < datetime.date.today():
@@ -32,7 +32,7 @@ class RenewBookModelForm(ModelForm):
         if data < datetime.date.today():
             raise ValidationError(_('Invalid date - renewal in past'))
 
-        # Check if a date is in the allowed range (+4 weeks from today).
+        # # Check if a date is in the allowed range (+4 weeks from today).
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
             raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
 
@@ -44,4 +44,3 @@ class RenewBookModelForm(ModelForm):
         fields = ['due_back']
         labels = {'due_back': _('Renewal date')}
         help_texts = {'due_back': _('Enter a date between now and 4 weeks (default 3).')} 
-
